2fb90f04649268f63ce95c9a5785187c52abf2b8
Bruno Winkeler <bruno.winkeler@virtus.ufcg.edu.br>
2022-12-13 14:15:09 -0300

    Add RestTemplate

---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/entities/Worker.java
@@ -0,0 +1,19 @@
+package com.devsuperior.hrpayroll.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Worker implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+}
